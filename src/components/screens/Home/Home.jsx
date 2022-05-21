a80d3334876fa1d941f9dd6f8439ff5538adab44
SkyLightToday <yulya.kolechkina83648@yandex.ru>
2022-05-21 18:38:45 +0300

    two

--- src/components/screens/Home/Home.jsx
+++ src/components/screens/Home/Home.jsx
@@ -1,8 +1,23 @@
 import React from "react";
 
+const todos = [
+    {
+        title: 'Finish the essay collaboration',
+        isCompleted: false,
+    },
+    {
+        title: 'Read next chapter of the book',
+        isCompleted: false,
+    },
+    {
+        title: 'Send the finished assignment',
+        isCompleted: false,
+    },
+]
+
 const Home = () => {
     return (
-        <div>
+        <div className='bg-gray-900 h-screen text-white'>
             Home
         </div>
     )
